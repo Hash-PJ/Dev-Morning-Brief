8d9028c8a119c90d9f051398d47ae5d052661a12
Hash-PJ <62370983+Hash-PJ@users.noreply.github.com>
2026-05-12 03:54:56 +0530

    Adding Joke feature

---
+++ sources/joke.py
@@ -0,0 +1,15 @@
+from sources.base import BaseSource
+
+
+class JokeSource(BaseSource):
+    URL = "https://v2.jokeapi.dev/joke/programming"
+
+    def fetch(self):
+        """Returns Joke as a string"""
+        data = self.get(self.URL)
+        if not data:
+            return None
+        if data.get('type') == 'single':
+            return data.get('joke')
+        if data.get('type')=='twopart':
+            return data.get('setup')+'\n||'+data.get('delivery')
